f8daedd341de5e74d5d4879626f7f54625380ac4
Khaled Osama <khaledosama@Khaleds-MacBook-Air.local>
2023-07-23 17:20:07 +0300

    complete the doctor card

--- lib/ui/widgets/doctors_card.dart
+++ lib/ui/widgets/doctors_card.dart
@@ -59,9 +59,57 @@ class DoctorCardState extends State<DoctorCard> {
             Text(
               widget.doctor.name,
               style: const TextStyle(
-                  fontSize: 11,
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white),
+                  color: Colors.black),
+            ),
+            const SizedBox(width: 4),
+            const SizedBox(width: 4),
+            Text(
+              '${widget.doctor.specialty}',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            Row(
+              children: [
+                // Icon(FlutterIcons.riyal_sign, size: 16, color: Colors.black),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 20,
+                      color: Colors.yellow,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '${widget.doctor.rate} ',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  '    سعر الكشف :  ${widget.doctor.sessionPrice} ريال ',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+              ],
             ),
           ],
         ),
